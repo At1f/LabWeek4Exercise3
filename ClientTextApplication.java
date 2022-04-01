de96330133f27d61e2c6859282018dddecc50713
At1f <102752871+At1f@users.noreply.github.com>
2022-04-01 08:19:45 +0800

    Add files via upload

---
+++ ClientTextApplication.java
@@ -0,0 +1,48 @@
+import java.io.BufferedReader;
+import java.io.DataInputStream;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.InetAddress;
+import java.net.Socket;
+import java.net.UnknownHostException;
+
+/**
+ * This method launch the frame and manage the connection to the server.
+ * 
+ * @author atif hazim 
+ * 
+ *
+ */
+
+public class ClientTextApplication {
+
+	public static void main(String[] args) 
+			throws UnknownHostException, IOException {
+		
+		// Launch client-side frame
+		ClientTextFrame clientTextFrame = new ClientTextFrame();
+		clientTextFrame.setVisible(true);
+		
+		// Connect to the server @ localhost, port 4338
+		Socket socket = new Socket(InetAddress.getLocalHost(), 4338);
+		
+		// Update the status of the connection
+		clientTextFrame.updateConnectionStatus(socket.isConnected());
+		
+		// Read from network
+		//BufferedReader bufferedReader = new BufferedReader(
+			//	new InputStreamReader(socket.getInputStream()));
+		
+		DataInputStream inputstream = new DataInputStream(socket.getInputStream());
+		
+		// Display the total words
+		int totalword  = inputstream.readInt();
+		clientTextFrame.updateText(totalword);
+		
+		// Close everything
+		//bufferedReader.close();
+		socket.close();
+
+	}
+
+}
